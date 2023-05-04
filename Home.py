1fc19a5c41f4661a564c9476d39aed83855f9b9a
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-05-04 10:57:01 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,8 +9,8 @@ filepath = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "heart
 
 df=pd.read_csv(filepath)
 
-model_path = os.path.abspath(os.path.join(os.getcwd(), "resources", "models", "lmodel.pkl"))
-scaler_path = os.path.abspath(os.path.join(os.getcwd(), "resources", "models", "stscaler.pkl"))
+model_path = os.path.abspath(os.path.join(os.getcwd(), "resources", "models", "linmodel.pkl"))
+scaler_path = os.path.abspath(os.path.join(os.getcwd(), "resources", "models", "stascaler.pkl"))
 
 
 # load the pre-trained model and scaler from files
@@ -20,9 +20,9 @@ with open(scaler_path, 'rb') as f:
     scaler = pickle.load(f)
 
 # define a function to predict the likelihood of heart failure
-def predict_heart_failure(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal):
+def predict_heart_failure( cp, thalach, exang, oldpeak, slope, ca, thal):
     # scale the input data using the pre-trained scaler
-    input_data = np.array([age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]).reshape(1, -1)
+    input_data = np.array([ cp, thalach, exang, oldpeak, slope, ca, thal]).reshape(1, -1)
     scaled_data = scaler.transform(input_data)
 
     # make a prediction using the pre-trained logistic regression model
@@ -38,13 +38,13 @@ def app():
     st.title('Heart Failure Prediction')
 
     # add input widgets for heart health parameters
-    age = st.slider('Age', 18, 100, 50)
-    sex = st.selectbox('Sex', ['Male', 'Female'])
+    #age = st.slider('Age', 18, 100, 50)
+    #sex = st.selectbox('Sex', ['Male', 'Female'])
     cp = st.selectbox('Chest Pain Type', [0, 1, 2, 3])
-    trestbps = st.slider('Resting Blood Pressure (mm Hg)', 90, 200, 120)
-    chol = st.slider('Cholesterol (mg/dL)', 100, 600, 200)
-    fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dL', [0, 1])
-    restecg = st.selectbox('Resting Electrocardiographic Results', [0, 1, 2])
+    #trestbps = st.slider('Resting Blood Pressure (mm Hg)', 90, 200, 120)
+    #chol = st.slider('Cholesterol (mg/dL)', 100, 600, 200)
+    #fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dL', [0, 1])
+    #restecg = st.selectbox('Resting Electrocardiographic Results', [0, 1, 2])
     thalach = st.slider('Maximum Heart Rate Achieved (bpm)', 50, 250, 150)
     exang = st.selectbox('Exercise Induced Angina', [0, 1])
     oldpeak = st.slider('ST Depression Induced by Exercise', 0.0, 6.0, 1.0, 0.1)
@@ -53,7 +53,7 @@ def app():
     thal = st.selectbox('Thalassemia', [0, 1, 2, 3])
 
     # convert the sex input to numerical form (0 for male, 1 for female)
-    sex = 0 if sex == 'Male' else 1
+    #sex = 0 if sex == 'Male' else 1
 
     # add a button to make the prediction
     if st.button('Predict'):
