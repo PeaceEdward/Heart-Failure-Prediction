18e7685d8c9ca6a261935e1de560b0bece18bab0
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-05-04 12:40:15 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -60,7 +60,7 @@ def predict_heart_failure( cp, thalach, exang, oldpeak, slope, ca, thal):
     # add a button to make the prediction
 if st.button('Predict'):
 # predict the likelihood of heart failure
-    pred = predict_heart_failure( cp, thalach, exang, oldpeak, slope, ca, thal)
+    pred = predict_heart_failure(scaled_data)
 
     # display the prediction result
     st.write(f'The likelihood of heart failure is {pred:.2f}.')
